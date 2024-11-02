44c16896253b1eff69e8ec9a65b3ef7054cc2108
Kelvin Guerrero <kelvinguerrero2199@gmail.com>
2024-11-02 11:32:08 +0100

    Add: lightning css, header

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.kelvinguerrero.dev",
-  integrations: [preact()],
+    site: "https://blog.kelvinguerrero.dev",
+    integrations: [preact()],
+    vite: {
+        css: {
+            transformer: "lightningcss",
+        },
+    },
 });
