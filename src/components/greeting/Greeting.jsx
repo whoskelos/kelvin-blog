e84354970ef7b4ecb8e9f9dfa83fd38c94ceb790
macbook pro kelvin guerrero <kelvinguerrero2199@gmail.com>
2024-11-20 20:00:21 +0100

    add: action to the button

--- src/components/greeting/Greeting.jsx
+++ src/components/greeting/Greeting.jsx
@@ -1,11 +1,21 @@
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import "./greeting.css";
 export default function Greeting() {
     const [isOpen, setIsOpen] = useState(false);
+    const showPet = () => {
+        console.log("SHOW PET HERE");
+        setIsOpen(false);
+    }
 
+    useEffect(() => {
+        console.log(isOpen);
+        if (isOpen) {
+            showPet();
+        }
+    },[isOpen])
     return (
-        <button className="greeting" onClick={() => setIsOpen(!isOpen)}>
-            Hi!
+        <button type="button" className="greeting" onClick={() => setIsOpen(!isOpen)}>
+            👋🏻
         </button>
     );
 }
